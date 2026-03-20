53cc4bafa4e8e639d51d06a600b1061852fc7797
Dmitry Lazenyuk <lazenyukdima@gmail.com>
2026-03-20 13:33:39 +0500

    Add login errors show and validate input

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,21 +4,51 @@ import { SubmitEvent, useState } from 'react';
 import styles from './page.module.scss';
 import { Input, Button } from '@/components/ui';
 import { useAuthStore } from '@/store/useAuthStore';
+import { AxiosError } from 'axios';
 
 export default function LoginPage() {
   const [form, setForm] = useState({ username: '', password: '' });
-  const [error, setError] = useState('');
+  const [errors, setErrors] = useState({ username: '', password: '' });
+  const [apiError, setApiError] = useState('');
+  const [isLoasing, setIsLoading] = useState(false);
 
   const login = useAuthStore((state) => state.login);
 
-  const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (form.username.length < 3 || form.password.length < 3) {
-      setError('Minimum 3 characters required');
-      return;
+  const validate = () => {
+    const newErrors = { username: '', password: '' };
+    let isValid = true;
+
+    if (form.username.length < 3) {
+      newErrors.username = 'Username must be at least 3 characters';
+      isValid = false;
     }
-    setError('');
-    login(form);
+
+    if (form.password.length < 3) {
+      newErrors.password = 'Password must be at least 3 characters';
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const handleSubmit = async (e: SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (validate()) {
+      try {
+        setIsLoading(true);
+        await login(form);
+        setApiError('');
+      } catch (e) {
+        if (e instanceof AxiosError) {
+          setApiError(e.response?.data.message);
+        }
+        throw e;
+      } finally {
+        setIsLoading(false);
+      }
+    }
   };
 
   return (
@@ -29,14 +59,23 @@ export default function LoginPage() {
           <Input
             placeholder="Username"
             value={form.username}
+            error={errors.username}
+            disabled={isLoasing}
             onChange={(e) => setForm({ ...form, username: e.target.value })}
           />
           <Input
             placeholder="Password"
             value={form.password}
+            error={errors.password}
+            disabled={isLoasing}
             onChange={(e) => setForm({ ...form, password: e.target.value })}
           />
-          <Button type="submit">Login</Button>
+          {apiError &&
+            <div className={styles.loginError}>
+              {apiError}
+            </div>
+          }
+          <Button type="submit" isLoading={isLoasing}>Login</Button>
         </form>
       </div>
     </section>
